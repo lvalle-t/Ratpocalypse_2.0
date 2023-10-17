6a169d2fe745c568f384baa4482c88d2db4e42e0
lvalle_t <lupitavalle713@gmail.com>
2023-10-16 20:34:56 -0500

    ant_healthbar

--- Assets/Scripts/ant_mob/ant_health.cs
+++ Assets/Scripts/ant_mob/ant_health.cs
@@ -1,4 +1,55 @@
-// // https://www.youtube.com/watch?v=BLfNP4Sc_iA&t=626s
+// // // https://www.youtube.com/watch?v=BLfNP4Sc_iA&t=626s
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// public class ant_health : MonoBehaviour
+// {
+
+//     public int maxHealth = 5;
+//     public int currentHealth;
+//     public HealthBar healthBar;
+
+//     private bool isDead;
+
+//     public GameManagerScript gameManager;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         currentHealth = maxHealth;
+//         // healthBar.SetMaxHealth(maxHealth);
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         // testing area//
+//         if (Input.GetKeyDown(KeyCode.Space))
+//         {
+//             TakeDamage(1);
+//         }
+//         // testing area///
+
+//         if (currentHealth <= 0 && !isDead)
+//         {
+//             isDead = true;
+//             Debug.Log("I am Dead!");
+//             Destroy(gameObject);
+//         }
+        
+//     }
+
+//     public void TakeDamage(int damage)
+//     {
+//         currentHealth -= damage;
+//         healthBar.SetHealth(currentHealth);
+//     }
+
+// }
+
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,43 +58,38 @@ using UnityEngine.UI;
 public class ant_health : MonoBehaviour
 {
 
-    public int maxHealth = 5;
-    public int currentHealth;
-    public HealthBar healthBar;
+    public int Hitpoints;
+    public int MaxHitpoints=5;
+    public ant_healthbar Healthbar;
 
-    private bool isDead;
-
-    public GameManagerScript gameManager;
-    // Start is called before the first frame update
-    void Start()
-    {
-        currentHealth = maxHealth;
-        // healthBar.SetMaxHealth(maxHealth);
+    void Start(){
+        Hitpoints = MaxHitpoints;
+        Healthbar.SetHealth(Hitpoints, MaxHitpoints);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        // testing area//
+
+    // testing area//
+    void Update(){
         if (Input.GetKeyDown(KeyCode.Space))
         {
             TakeDamage(1);
         }
         // testing area///
-
-        if (currentHealth <= 0 && !isDead)
-        {
-            isDead = true;
-            Debug.Log("I am Dead!");
+    }
+    
+    public void TakeDamage(int damage){
+        Hitpoints -= damage;
+        Healthbar.SetHealth(Hitpoints, MaxHitpoints);
+        if(Hitpoints <= 0){
             Destroy(gameObject);
         }
-        
     }
 
-    public void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
-        healthBar.SetHealth(currentHealth);
-    }
+    // public void TakeDamage(int damage)
+    // {
+    //     Hitpoints -= damage;
+    //     HealthBar.SetHealth(Hitpoints);
+    // }
 
+    
 }
---
+++ Assets/Scripts/ant_mob/ant_healthbar.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Add this line
+
+
+public class ant_healthbar : MonoBehaviour
+{
+    public Slider Slider;
+    public Color Low;
+    public Color High;
+    public Vector3 Offset = new Vector3(0f, 1.5f, 0f); // Example offset to place the health bar above the mob's head
+
+
+
+    // Start is called before the first frame update
+    public void SetHealth(float health, float maxHealth)
+    {
+        Slider.gameObject.SetActive(health<maxHealth);
+        Slider.value= health;
+        Slider.maxValue =maxHealth;
+
+        Slider.fillRect.GetComponentInChildren<Image>().color=Color.Lerp(Low,High,Slider.normalizedValue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Slider.transform.position=Camera.main.WorldToScreenPoint(transform.parent.position+Offset);
+    }
+}
