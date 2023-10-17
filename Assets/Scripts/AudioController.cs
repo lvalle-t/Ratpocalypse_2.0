92a973e2898bb8362a34ecb08b20fe9f0b867812
ayyfabes <fabianalexandersilva@gmail.com>
2023-10-17 15:04:21 -0500

    Music now continues from scene to scene without stopping

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
-    private AudioSource audioSource;
+    private static AudioController instance;
 
-    void Start()
+    // Add a public static property to access the instance.
+    public static AudioController Instance
     {
-        audioSource = GetComponent<AudioSource>();
+        get { return instance; }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Prevents the object from being destroyed during scene changes.
+        }
+        else
+        {
+            Destroy(gameObject); // Destroy duplicate AudioControllers.
+        }
+    }
+
+    public void PlayMusic(AudioClip musicClip)
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
 
         if (audioSource != null)
         {
+            audioSource.clip = musicClip;
+            audioSource.loop = true;
             audioSource.Play();
         }
         else
