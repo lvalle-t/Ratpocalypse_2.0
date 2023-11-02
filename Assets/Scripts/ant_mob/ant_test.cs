45ab5f49acf39908860930facfe71aef7b700823
Hannah Hebert <hannah.hebert06@gmail.com>
2023-11-02 16:17:32 -0500

    added spider mob to sewer scene

--- Assets/Scripts/ant_mob/ant_test.cs
+++ Assets/Scripts/ant_mob/ant_test.cs
@@ -5,26 +5,26 @@ using UnityEngine;
 public class ant_test : MonoBehaviour
 {
     //// Start is called before the first frame update
-    //public float _health = 3;
-    //public float Health
-    //{
-    //    set
-    //    {
-    //        _health = value;
+    public float _health = 3;
+    public float Health
+    {
+       set
+       {
+           _health = value;
 
-    //        if (_health <= 0)
-    //        {
-    //            Destroy(gameObject);
-    //        }
-    //    }
-    //    get
-    //    {
-    //        return _health;
-    //    }
-    //}
-    //void TakeDamage(float damage)
-    //{
-    //    Debug.Log("Ant Hit " + damage);
-    //    Health -= damage;
-    //}
+           if (_health <= 0)
+           {
+               Destroy(gameObject);
+           }
+       }
+       get
+       {
+           return _health;
+       }
+    }
+    void TakeDamage(float damage)
+    {
+       Debug.Log("Ant Hit " + damage);
+       Health -= damage;
+    }
 }
