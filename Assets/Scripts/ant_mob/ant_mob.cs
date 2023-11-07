5686db651972c3053f70416565310e5600a96e34
Anthony Castillo <508ajc@gmail.com>
2023-11-06 18:11:56 -0600

    updated mob spawner and fixed the player

--- Assets/Scripts/ant_mob/ant_mob.cs
+++ Assets/Scripts/ant_mob/ant_mob.cs
@@ -65,24 +65,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class ant_mob : MonoBehaviour
 {
-    // public float speed; 
-    // public float stoppingDistance;
-    private float timeBtwShots;
-    public float startTimeBtwShots;
+    private AIPath aiPath; // Reference to the AIPath component.
     public GameObject fire_breath;
-    public Transform player;
+    private Transform player;
+
+    public float startTimeBtwShots;
+    private float timeBtwShots;
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        // Check if AIPath component is already attached, if not, add it.
+        aiPath = GetComponent<AIPath>();
+        if (aiPath == null)
+        {
+            aiPath = gameObject.AddComponent<AIPath>();
+        }
+
+        player = GameObject.FindGameObjectWithTag("Player").transform; // Assuming your player has the "Player" tag.
         timeBtwShots = startTimeBtwShots;
+
+        // Set the AIPath's destination to the player's position.
+        //aiPath.destination = player.position;
     }
 
     void Update()
     {
+        // Update the AIPath's destination to follow the player.
+        aiPath.destination = player.position;
+
         if (timeBtwShots <= 0)
         {
             Instantiate(fire_breath, transform.position, Quaternion.identity);
