c9291b28d7a0d53127624018101a223e3eb10784
lvalle_t <lupitavalle713@gmail.com>
2023-11-09 15:13:13 -0600

    healthbars

--- Assets/Scripts/ant_mob/ant_test.cs
+++ Assets/Scripts/ant_mob/ant_test.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ant_test : MonoBehaviour
 {
     //// Start is called before the first frame update
     public float _health = 3;
+    public int Hitpoints;
+    public spider_healthbar Healthbar;
+
     public float Health
     {
        set
