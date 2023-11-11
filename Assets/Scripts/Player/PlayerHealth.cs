9458218a8c5f29944a21682e97e0f66d0a5ee4a4
lvalle_t <lupitavalle713@gmail.com>
2023-11-10 20:20:47 -0600

    game over screen working in sewer

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -8,6 +8,10 @@ public class PlayerHealth : MonoBehaviour
 {
     public HealthBar healthBar;
 
+    public GameManagerScript gameManager;
+
+    private bool isDead;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,16 +24,33 @@ public class PlayerHealth : MonoBehaviour
         
     }
 
+    // public void EnemyDamage(float damage)
+    // {
+    //     if ((updater.playerHp - damage) > 0f)
+    //     {
+    //         updater.playerHp -= damage;
+    //     }
+    //     else
+    //     {
+    //         updater.playerHp = 0f;
+    //     }
+    //     healthBar.UpdateHealth(updater.playerHp);
+    // }
+
     public void EnemyDamage(float damage)
     {
-        if ((updater.playerHp - damage) > 0f)
-        {
-            updater.playerHp -= damage;
-        }
-        else
-        {
-            updater.playerHp = 0f;
-        }
+        updater.playerHp -= damage;
+
         healthBar.UpdateHealth(updater.playerHp);
+
+        if (updater.playerHp <= 0f && !isDead)
+        {   
+            updater.playerHp = 0f;
+            isDead = true;
+            gameManager.gameOver();
+            Debug.Log("Player is dead!"); // Print "dead" message to the console
+        }
     }
+
+
 }
