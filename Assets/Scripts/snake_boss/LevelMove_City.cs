1835d9dbacc96d3802d91440be2a0d9baf77e1c1
Tristan Fry <tristanfry1234@gmail.com>
2023-11-12 18:19:33 -0600

    add door transtion to sewer scene

---
+++ Assets/Scripts/snake_boss/LevelMove_City.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMove_City : MonoBehaviour
+{
+    public int sceneBuildIndex;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
+    }
+}
