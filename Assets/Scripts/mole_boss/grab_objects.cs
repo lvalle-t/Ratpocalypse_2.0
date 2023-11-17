ba0d569f16b8ccccf4945b854e7c0a63c2aaf236
Deb <debauer@uh.edu>
2023-11-17 02:43:48 -0600

    player picks up keys and holds in hand, added a keycard to enter Petcorp, must kill correct spider to get it

---
+++ Assets/Scripts/mole_boss/grab_objects.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class grab_objects : MonoBehaviour
+{
+    [SerializeField] private Transform grabPoint;
+    [SerializeField] private Transform rayPoint;
+    [SerializeField] private float rayDistance;
+
+    private GameObject grabbedObject;
+    private int layerIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        layerIndex = LayerMask.NameToLayer("Key");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, transform.right, rayDistance);
+
+        if (hitInfo.collider != null && hitInfo.collider.gameObject.layer == layerIndex) 
+        {
+            if (Keyboard.current.spaceKey.wasPressedThisFrame && grabbedObject == null)
+            {
+                grabbedObject = hitInfo.collider.gameObject;
+                //grabbedObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                grabbedObject.transform.position = grabPoint.position;
+                grabbedObject.transform.SetParent(transform);
+            }
+            else if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            {
+                //grabbedObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                grabbedObject.transform.SetParent(null);
+                grabbedObject = null;
+            }
+        }
+    }
+}
