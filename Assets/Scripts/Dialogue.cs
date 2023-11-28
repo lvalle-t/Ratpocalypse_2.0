0258699d4dbff46c34e2f0ab1fb45b5bf8fc7490
lvalle_t <lupitavalle713@gmail.com>
2023-11-28 12:46:44 -0600

    Lab dialogue

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -1,80 +1,3 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using TMPro;
-
-// public class Dialogue : MonoBehaviour
-// {
-//     public TextMeshPro textComponent;
-//     public string[] lines;
-//     public float textSpeed;
-//     private int index = 0;
-//     public Animator dialogueAnimator;
-//     private bool StartDialogue = true;
-//     public GameObject startPlayer;
-//     public GameObject startMap;
-
-//     // Start is called before the first frame update
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         if (Input.GetKeyDown(KeyCode.Space))
-//         {
-//             if (StartDialogue)
-//             {
-//                 dialogueAnimator.SetTrigger("enter");
-//                 StartDialogue = false;
-//             }
-//             else
-//             {
-//                 NextLine();
-//             }
-
-//         }
-//     }
-
-//     IEnumerator TypeLine()
-//     {
-//         foreach (char c in lines[index].ToCharArray())
-//         {
-//             textComponent.text += c;
-//             yield return new WaitForSeconds(textSpeed);
-//         }
-//         index++;
-//     }
-
-//     void NextLine()
-//     {
-//         if (index <= lines.Length - 1)
-//         {
-//             textComponent.text = string.Empty;
-//             StartCoroutine(TypeLine());
-//         }
-//         else
-//         {
-//             textComponent.text = string.Empty;
-//             dialogueAnimator.SetTrigger("exit");
-//             // Call the StartLevel function to turn on objects
-//             StartLevel();
-//         }
-//     }
-
-//     public void StartLevel()
-//     {
-//         startPlayer.SetActive(true);
-//         startMap.SetActive(true);
-//     }
-
-//     public void CloseBox()
-//     {
-//         startPlayer.SetActive(false);
-//         startMap.SetActive(false);
-//     }
-// }
-
-
-
 using System.Collections;
 using UnityEngine;
 using TMPro;
@@ -88,6 +11,9 @@ public class Dialogue : MonoBehaviour
     public Animator dialogueAnimator;
     public GameObject startPlayer;
     public GameObject startMap;
+
+    // Add a skip button
+    public KeyCode skipButton = KeyCode.Escape;
 
     // Start is called before the first frame update
     void Start()
@@ -113,6 +39,12 @@ public class Dialogue : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             NextLine();
+        }
+
+        // Check for skip input
+        if (Input.GetKeyDown(skipButton))
+        {
+            SkipDialogue();
         }
     }
 
@@ -141,6 +73,14 @@ public class Dialogue : MonoBehaviour
             dialogueAnimator.SetTrigger("exit");
             StartLevel();
         }
+    }
+
+    void SkipDialogue()
+    {
+        // Skip the dialogue and start the level immediately
+        textComponent.text = string.Empty;
+        dialogueAnimator.SetTrigger("exit");
+        StartLevel();
     }
 
     void StartLevel()
