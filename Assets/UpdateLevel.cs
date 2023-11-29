f0c59fb4cf995adfd1d8d9f9face4b356cdf96c6
Anthony Castillo <508ajc@gmail.com>
2023-11-28 20:24:20 -0600

    fixed mobs in outside and store scene

---
+++ Assets/UpdateLevel.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Text levelTxt;
+    [SerializeField] LevelProgression lp;          // assignes the text and updates the count - deb
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelTxt = GetComponent<Text>();        // gets access to text in treat component - deb
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        levelTxt.text = lp.CurrLevel.ToString();    // assigning the text and counter updater before adding - deb
+    }
+}
