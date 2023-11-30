78593ef0ee853041edf6e76332834ac72699e9c7
Deb <debauer@uh.edu>
2023-11-29 22:20:30 -0600

    created new sprites for the alligator boss, changed the sprite, created animation, and changed the code

--- Assets/Scripts/alligator_boss/Alligator_Attack.cs
+++ Assets/Scripts/alligator_boss/Alligator_Attack.cs
@@ -6,47 +6,16 @@ public class Alligator_Attack : MonoBehaviour
 {
     public Animator gatorAnimator;
 
-    public float tailDamage = 1f;
     public float biteDamage = 2f;
     public Vector3 attackOffset;
     public float biteAttackRange;
-    public float tailAttackRange;
     public LayerMask attackMask;
-
-    public float speed = 1f;
-    float cooldownTimer = 5f;
-    bool onCooldown = false;
-
-
     
-    public void TailAttack()
-    {
-        if (!onCooldown)
-        {
-            Vector3 pos = transform.position;
-            pos += transform.right * attackOffset.x;
-            pos += transform.up * attackOffset.y;
 
-            Collider2D colInfo = Physics2D.OverlapCircle(pos, tailAttackRange, attackMask);
-            if (colInfo != null)
-            {
-                colInfo.GetComponent<PlayerHealth>().EnemyDamage(tailDamage);
-            }
-        }
-        else
-        {
-            onCooldown = true;
-
-            cooldownTimer -= Time.deltaTime;
-            if (cooldownTimer <= 0)
-            {
-                onCooldown = false;
-                cooldownTimer = 5;
-            }
-        }
-    }
     public void BiteAttack()
     {
+        gatorAnimator.SetTrigger("isAttacking");
+
         Vector3 pos = transform.position;
         pos += transform.right * attackOffset.x;
         pos += transform.up * attackOffset.y;
@@ -60,7 +29,6 @@ public class Alligator_Attack : MonoBehaviour
 
     public void ExitAttack()
     {
-        gatorAnimator.ResetTrigger("Alligator_bite");
-        gatorAnimator.ResetTrigger("Alligator_tail_swipe");
+        gatorAnimator.ResetTrigger("isAttacking");
     }
 }
